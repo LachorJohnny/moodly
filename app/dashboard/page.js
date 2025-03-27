b966d1d3915559d9dfa702ab69fe6be850fdbc30
Jonatan Lachor <lachorjonatan@icloud.com>
2025-03-27 14:17:15 +0100

    Add basic configuration and route structure with Main component

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+import Main from "@/components/Main";
+
+export const metadata = {
+  title: 'Moodly · Dashboard',
+};
+
+export default function DashboardPage() {
+  return (
+    <Main>
+      
+    </Main>
+  )
+}
