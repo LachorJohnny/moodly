e3b3bc01f88efe928950ebe8ad6a87a82573e527
Jonatan Lachor <lachorjonatan@icloud.com>
2025-03-30 18:59:28 +0200

    Add hero section to home page

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Fugaz_One } from 'next/font/google';
+import { Fugaz_One, Open_Sans } from 'next/font/google';
 import './globals.css';
 
 const fugazOne = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
+const openSans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Moodly',
@@ -13,13 +14,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`w-full w-max-[1000px] min-h-screen mx-auto text-sm sm:text-base
-         flex flex-col antialiased`}
+         flex flex-col text-slate-800 ${openSans.className}`}
       >
         <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
           <h1 className={'text-base sm:text-lg textGradient ' + fugazOne.className}>Moodly</h1>
         </header>
         {children}
-        <footer className='p-4 sm:p-8'>footer</footer>
+        <footer className="p-4 sm:p-8 grid place-items-center">
+          <p className={'text-indigo-500 ' + fugazOne.className}>Created with 💜</p>
+        </footer>
       </body>
     </html>
   );
---
+++ components/Button.jsx
@@ -0,0 +1,19 @@
+import { Fugaz_One } from 'next/font/google';
+import React from 'react';
+
+const fugazOne = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
+
+export default function Button(props) {
+  const { text, dark } = props;
+
+  return (
+    <button
+      className={
+        'rounded-full overflow-hidden duration-200 hover:opacity-60 border-2 border-solid border-indigo-600 ' +
+        (dark ? 'text-white bg-indigo-600 border-indigo-600' : 'text-indigo-600 ')
+      }
+    >
+      <p className={'px-6 sm:px-10 py-2 sm:py-3 whitespace-nowrap ' + fugazOne.className}>{text}</p>
+    </button>
+  );
+}
