22779e982347fc065bde602afde4591e03c0e151
Jonatan Lachor <lachorjonatan@icloud.com>
2025-04-08 21:45:29 +0200

    Add AuthContext with full auth logic and Firestore user data handling

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Fugaz_One, Open_Sans } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
+import { AuthProvider } from '@/context/AuthContext';
 
 const fugazOne = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 const openSans = Open_Sans({ subsets: ['latin'] });
@@ -13,21 +14,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`w-full w-max-[1000px] min-h-screen mx-auto text-sm sm:text-base
-         flex flex-col text-slate-800 ${openSans.className}`}
-      >
-        <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
-          <Link href={'/'}>
-            <h1 className={'text-base sm:text-lg textGradient ' + fugazOne.className}>Moodly</h1>
-          </Link>
-          <div className="flex items-center justify-between">PLACEHOLDER CTA || STATS</div>
-        </header>
-        {children}
-        <footer className="p-4 sm:p-8 grid place-items-center">
-          <p className={'text-indigo-500 ' + fugazOne.className}>Created with 💜</p>
-        </footer>
-      </body>
+      <AuthProvider>
+        <body
+          className={`w-full w-max-[1000px] min-h-screen mx-auto text-sm sm:text-base
+          flex flex-col text-slate-800 ${openSans.className}`}
+        >
+          <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+            <Link href={'/'}>
+              <h1 className={'text-base sm:text-lg textGradient ' + fugazOne.className}>Moodly</h1>
+            </Link>
+            <div className="flex items-center justify-between">PLACEHOLDER CTA || STATS</div>
+          </header>
+          {children}
+          <footer className="p-4 sm:p-8 grid place-items-center">
+            <p className={'text-indigo-500 ' + fugazOne.className}>Created with 💜</p>
+          </footer>
+        </body>
+      </AuthProvider>
     </html>
   );
 }
---
+++ context/AuthContext.js
@@ -0,0 +1,80 @@
+'use client';
+
+import { auth, db } from '@/firebase';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useContext, useState, useEffect } from 'react';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userDataObj, setUserDataObj] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  // AUTH HANDLERS
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    setCurrentUser(null);
+    setUserDataObj({});
+    return signOut();
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      try {
+        setLoading(true);
+        setCurrentUser(user);
+        if (!user) {
+          return;
+        }
+
+        // If user exist, fetch data from firestore database
+        const docRef = doc(db, 'users', user.uid);
+        const docSnap = await getDoc(docRef);
+        let firebaseData = {};
+
+        if (docSnap.exists()) {
+          firebaseData = docSnap.data();
+        }
+
+        setUserDataObj(firebaseData);
+      } catch (err) {
+        console.error(err.message);
+      } finally {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  const value = {
+    currentUser,
+    userDataObj,
+    loading,
+    signup,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
