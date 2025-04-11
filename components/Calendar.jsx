54821a0f20419cf98cc5e52d2410ace3b6779196
Jonatan Lachor <lachorjonatan@icloud.com>
2025-04-11 21:27:55 +0200

    Add nav for calendar component

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -2,6 +2,9 @@
 
 import React, { useState } from 'react';
 import { gradients, baseRating } from '@/utils';
+import { Fugaz_One } from 'next/font/google';
+
+const fugazOne = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 
 const months = {
   January: 'Jan',
@@ -18,7 +21,6 @@ const months = {
   December: 'Dec',
 };
 const monthsArr = Object.keys(months);
-const now = new Date();
 const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 export default function Calendar(props) {
@@ -27,64 +29,95 @@ export default function Calendar(props) {
   const now = new Date();
   const currentMonth = now.getMonth();
   const [selectedYear, setSelectedYear] = useState(now.getFullYear());
-  const [selectedMonth, setSelectedMonth] = useState(Object.keys(months)[currentMonth]);
+  const [selectedMonth, setSelectedMonth] = useState(monthsArr[currentMonth]);
 
-  const numericMonth = Object.keys(months).indexOf(selectedMonth);
+  const numericMonth = monthsArr.indexOf(selectedMonth);
   const data = completeData?.[selectedYear]?.[numericMonth] || {};
 
   function handleIncrementMonth(val) {
-    
+    if (numericMonth + val < 0) {
+      setSelectedYear((curr) => curr - 1);
+      setSelectedMonth(monthsArr[monthsArr.length - 1]);
+    } else if (numericMonth + val > 11) {
+      setSelectedYear((curr) => curr + 1);
+      setSelectedMonth(monthsArr[0]);
+    } else {
+      setSelectedMonth(monthsArr[numericMonth + val]);
+    }
   }
 
-  const monthNow = new Date(selectedYear, Object.keys(months).indexOf(selectedMonth), 1);
+  const monthNow = new Date(selectedYear, monthsArr.indexOf(selectedMonth), 1);
   const firstDayOfMonth = monthNow.getDay();
-  const daysInMonth = new Date(selectedYear, Object.keys(months).indexOf(selectedMonth) + 1, 0).getDate();
+  const daysInMonth = new Date(selectedYear, monthsArr.indexOf(selectedMonth) + 1, 0).getDate();
 
   const daysToDisplay = firstDayOfMonth + daysInMonth;
   const numRows = Math.floor(daysToDisplay / 7) + (daysToDisplay % 7 ? 1 : 0);
 
   return (
-    <div className="flex flex-col overflow-hidden gap-1 py-4 sm:py-6 md:py-10">
-      {[...Array(numRows).keys()].map((row, rowIndex) => {
-        return (
-          <div key={rowIndex} className="grid grid-cols-7 gap-1">
-            {dayList.map((dayOfWeek, dayOfWeekIndex) => {
-              let dayIndex = rowIndex * 7 + dayOfWeekIndex - (firstDayOfMonth - 1);
-              let dayDisplay =
-                dayIndex > daysInMonth
-                  ? false
-                  : row === 0 && dayOfWeekIndex < firstDayOfMonth
-                  ? false
-                  : true;
-              let isToday = dayIndex === now.getDate();
+    <div className="flex flex-col gap-4">
+      <div className="grid grid-cols-5 gap-4">
+        <button
+          onClick={() => handleIncrementMonth(-1)}
+          className="mr-auto text-indigo-400 text-lg sm:text-xl duration-200 hover:opacity-60"
+        >
+          <i className="fa-solid fa-circle-chevron-left" />
+        </button>
+        <p
+          className={
+            'text-center capitalize whitespace-nowrap textGradient col-span-3 ' + fugazOne.className
+          }
+        >
+          {selectedMonth}, {selectedYear}
+        </p>
+        <button
+          onClick={() => handleIncrementMonth(+1)}
+          className="ml-auto text-indigo-400 text-lg sm:text-xl duration-200 hover:opacity-60"
+        >
+          <i className="fa-solid fa-circle-chevron-right" />
+        </button>
+      </div>
+      <div className="flex flex-col overflow-hidden gap-1 py-4 sm:py-6 md:py-10">
+        {[...Array(numRows).keys()].map((row, rowIndex) => {
+          return (
+            <div key={rowIndex} className="grid grid-cols-7 gap-1">
+              {dayList.map((dayOfWeek, dayOfWeekIndex) => {
+                let dayIndex = rowIndex * 7 + dayOfWeekIndex - (firstDayOfMonth - 1);
+                let dayDisplay =
+                  dayIndex > daysInMonth
+                    ? false
+                    : row === 0 && dayOfWeekIndex < firstDayOfMonth
+                    ? false
+                    : true;
+                let isToday = dayIndex === now.getDate();
 
-              if (!dayDisplay) {
-                return <div key={dayOfWeekIndex} className="bg-white" />;
-              }
+                if (!dayDisplay) {
+                  return <div key={dayOfWeekIndex} className="bg-white" />;
+                }
 
-              let color = demo
-                ? gradients.indigo[baseRating[dayIndex]]
-                : dayIndex in data
-                ? gradients.indigo[data[dayIndex]]
-                : 'white';
+                let color = demo
+                  ? gradients.indigo[baseRating[dayIndex]]
+                  : dayIndex in data
+                  ? gradients.indigo[data[dayIndex]]
+                  : 'white';
 
-              return (
-                <div
-                  key={dayOfWeekIndex}
-                  style={{ background: color }}
-                  className={
-                    'text-xs sm:text-sm border border-solid p-2 flex items-center justify-between gap-2 rounded-lg ' +
-                    (isToday ? ' border-indigo-400' : ' border-indigo-100') +
-                    (color === 'white' ? ' text-indigo-400' : ' text-white')
-                  }
-                >
-                  <p>{dayIndex}</p>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+                return (
+                  <div
+                    key={dayOfWeekIndex}
+                    style={{ background: color }}
+                    className={
+                      'text-xs sm:text-sm border border-solid p-2 flex items-center justify-between gap-2 rounded-lg ' +
+                      (isToday ? ' border-indigo-400' : ' border-indigo-100') +
+                      (color === 'white' ? ' text-indigo-400' : ' text-white')
+                    }
+                  >
+                    <p>{dayIndex}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
